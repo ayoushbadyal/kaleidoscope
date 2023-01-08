43195924d9400ab7b15f681bd467b8befbd2514f
ayoushbadyal <ayoushbadyal@gmail.com>
2023-01-08 21:50:57 +0530

    adding span for tokens ..

--- src/token.rs
+++ src/token.rs
@@ -1,30 +1,64 @@
-use std::fmt::Display;
+use std::ops::Range;
+
+#[derive(Debug, Clone)]
+pub struct TokenT {
+  pub kind: Token,
+  pub span: Span,
+}
+
+#[derive(Debug, Clone)]
+pub struct Span {
+  pub start: usize,
+  pub end: usize,
+}
+
+impl From<Range<usize>> for Span {
+  fn from(a: Range<usize>) -> Self {
+    Self {
+      start: a.start,
+      end: a.end,
+    }
+  }
+}
+
+impl From<Span> for Range<usize> {
+  fn from(a: Span) -> Self {
+    Range {
+      start: a.start,
+      end: a.end,
+    }
+  }
+}
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Token {
-    Eof,
-    Def,
-    Extern,
-    Identifier(String),
-    Number(f64),
-    Char(char),
-    If,
-    Else,
-    Then,
-    //> if-token
-    For,
-    In,
-    Eq,
-    //> ch-4 for-token
-    Binary,
-    Unary,
-    //> ch-5
-    Var,
-    Oppar,
-    Clpar,
+  Eof,
+  Def,
+  Extern,
+  Identifier(String),
+  Number(f64),
+  Char(char),
+  If,
+  Else,
+  Then,
+  //> if-token
+  For,
+  In,
+  Eq,
+  //> ch-4 for-token
+  Binary,
+  Unary,
+  //> ch-5
+  Var,
+  Oppar,
+  Clpar,
+}
+macro_rules! token {
+  ($a:expr,$b:expr) => {
+    TokenT { kind: $a, span: $b }
+  };
 }
 
-#[macro_use]
 #[macro_export]
 macro_rules! T {
     [+] => {        $crate::token::Token::Char('+')};
@@ -60,90 +94,93 @@ macro_rules! T {
 
 #[derive(Debug)]
 pub struct Lexer {
-    input: Vec<char>,
+  input: Vec<char>,
 }
 
 impl Lexer {
-    pub fn new(inp: &str) -> Self {
-        Self {
-            input: inp.chars().collect::<Vec<char>>(),
-        }
+  pub fn new(inp: &str) -> Self {
+    Self {
+      input: inp.chars().collect::<Vec<char>>(),
     }
+  }
 
-    pub fn get_token(&self) -> Vec<Token> {
-        use Token::*;
-        let mut i = 0;
-        let mut res = Vec::new();
-        loop {
-            if i == self.input.len() {
-                res.push(T![EOF]);
+  pub fn get_token(&self) -> Vec<TokenT> {
+    use Token::*;
+    let mut i = 0;
+    let mut res = Vec::new();
+    loop {
+      if i == self.input.len() {
+        res.push(token!(T![EOF], Span::from(i..self.input.len())));
+        break;
+      }
+
+      match self.input[i] {
+        'a'..='z' | 'A'..='Z' => {
+          let mut aa = String::new();
+          let old = i;
+          loop {
+            if !(i < self.input.len() && self.input[i].is_alphabetic()) {
+              break;
+            }
+            aa.push(self.input[i]);
+            i += 1;
+          }
+
+          res.push(token!(
+            match aa.as_str() {
+              "def" => T![def],
+              "extern" => Extern,
+              "if" => If,
+              "then" => Then,
+              "else" => Else,
+              "for" => For,
+              "in" => In,
+              "binary" => Binary,
+              "unary" => Unary,
+              "var" => Var,
+              a @ _ => Identifier(a.into()),
+            },
+            (old..i).into()
+          ));
+        }
+        '0'..='9' => {
+          let mut aa = String::new();
+          let ol = i;
+          loop {
+            if !(i < self.input.len() && (self.input[i].is_digit(10) || self.input[i] == '.')) {
+              break;
+            }
+            aa.push(self.input[i]);
+            i += 1;
+          }
+          res.push(token!(Number(aa.parse::<f64>().unwrap()), (ol..i).into()));
+        }
+        ' ' | '\n' | '\r' => {
+          i += 1;
+        }
+        a @ _ => {
+          let old = i;
+          if a == '#' {
+            // ign comments
+            loop {
+              let ch = self.input[i];
+              if ch == '\r' || ch == '\n' {
+                i += 1;
                 break;
+              }
+              i += 1;
             }
-
-            match self.input[i] {
-                'a'..='z' | 'A'..='Z' => {
-                    let mut aa = String::new();
-                    loop {
-                        if !(i < self.input.len() && self.input[i].is_alphabetic()) {
-                            break;
-                        }
-                        aa.push(self.input[i]);
-                        i += 1;
-                    }
-
-                    match aa.as_str() {
-                        "def" => res.push(T![def]),
-                        "then" => res.push(T![then]),
-                        "extern" => res.push(Extern),
-                        "if" => res.push(If),
-                        "then" => res.push(Then),
-                        "else" => res.push(Else),
-                        "for" => res.push(For),
-                        "in" => res.push(In),
-                        "binary" => res.push(Binary),
-                        "unary" => res.push(Unary),
-                        "var" => res.push(Var),
-                        a @ _ => res.push(Identifier(a.into())),
-                    }
-                }
-                '0'..='9' => {
-                    let mut aa = String::new();
-                    loop {
-                        if !(i < self.input.len()
-                            && (self.input[i].is_digit(10) || self.input[i] == '.'))
-                        {
-                            break;
-                        }
-                        aa.push(self.input[i]);
-                        i += 1;
-                    }
-                    res.push(Number(aa.parse::<f64>().unwrap()));
-                }
-                ' ' | '\n' => {
-                    i += 1;
-                }
-                a @ _ => {
-                    if a == '#' {
-                        // ign comments
-                        loop {
-                            let ch = self.input[i];
-                            if ch == '\r' || ch == '\n' {
-                                i += 1;
-                                break;
-                            }
-                            i += 1;
-                        }
-                    } else if a == '{' {
-                        res.push(Oppar);
-                    } else if a == '}' {
-                        res.push(Clpar);
-                    } else {
-                        res.push(Char(a));
-                    }
-                    i += 1;
-                }
-            }
+          } else if a == '{' {
+            res.push(token!(Oppar, (old..i).into()));
+          } else if a == '}' {
+            res.push(token!(Clpar, (old..i).into()));
+          } else {
+            res.push(token!(Char(a), (old..i).into()));
+          }
+          i += 1;
         }
-        res
+      }
     }
+    res
+  }
 }
